b9f4943176a9e6e9873fed2a214276d18726d2ef
Logon Aluno <logonrmlocal@fiap.com.br>
2019-02-09 15:49:09 -0200

    ui changes for beauty and screen division for easier development

--- App.js
+++ App.js
@@ -1,89 +1,42 @@
 import React from 'react';
-import { StyleSheet, Text, View, TextInput, Image } from 'react-native';
-import { Button, List, ListItem } from 'native-base';
+import {
+  createStackNavigator,
+  createAppContainer,
+} from 'react-navigation';
+
+import ProfileScreen from './screens/Profile/Index';
+import SearchScreen from './screens/Search/Index';
+
+const AppNavigator = createStackNavigator(
+  {
+    Search: {
+      screen: SearchScreen,
+    },
+    Profile: {
+      screen: ProfileScreen,
+    },
+  },
+  {
+    initialRouteName: 'Search',
+    defaultNavigationOptions: {
+      headerStyle: {
+        backgroundColor: '#333',
+      },
+      headerTintColor: '#fff',
+      headerTitleStyle: {
+        fontWeight: 'bold',
+      },
+    },
+  }
+);
+
+//export default createAppContainer(AppNavigator);
+
+
+const AppContainer = createAppContainer(AppNavigator);
 
 export default class App extends React.Component {
-  state = {
-    error:false,
-    list:[],
-    imageUrl:''
-  };
-  
-
-  constructor(props){
-    super(props);
-    this.search = this.search.bind(this);
-  }
-
-  search(value){
-    console.log(`value:${value}`);
-    if(value.length > 3){
-      this.setState({list:[]});
-      fetch(`https://www.superheroapi.com/api.php/10216447089241794/search/${value}`)
-        .then((response) => response.json())
-        .then((data)=>{
-          if(data.error){
-           return this.setState({list:[], error:true, imageUrl:''});
-          }
-          return this.setState({list:data.results, imageUrl:data.results[0].image.url, error:false});
-        })
-        .catch((error)=>{console.log(error)});
-    }
-  }
-
-  renderError(){
-    if(this.state.error){
-      return(<Text>Deu ruim!</Text>)
-    }
-  }
-
-  renderImage(){
-    console.log(this.state.imageUrl);
-    if(this.state.imageUrl)
-      return(<Image 
-        source={{uri: this.state.imageUrl}}
-        style={{width: 200, height: 200}}
-      ></Image>)
-    else
-      return(<Image source=''></Image>)
-  }
-
-  renderList(){
-    if(this.state.list.length === 0){
-      return null;
-    }
-    return(<List>{
-      this.state.list.map((item) => (
-        <ListItem key={`item-${item.id}`}>
-            <Text>{item.id}</Text>
-            <Text>{item.name}</Text>
-        </ListItem>
-    ))
-    }</List>)
-  }
-
   render() {
-    return (
-      <View style={styles.container}>
-        {this.renderImage()}
-        <Text>Welcome to the App!</Text>
-        <TextInput
-          style={{height:40, width:200, borderColor:'gray', borderWidth: 1}}
-          onChangeText={this.search}>
-        </TextInput>
-        {this.renderList()}
-        {this.renderError()}
-      </View>
-    );
+    return (<AppContainer />);
   }
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#ddd',
-    color: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+};
---
+++ screens/Search/Index.js
@@ -0,0 +1,103 @@
+import React from 'react';
+//React Native
+import { StyleSheet, Text, View, TextInput } from 'react-native';
+//Native Base
+import { List, ListItem, Thumbnail, Right, Left, Icon, Body, Button } from 'native-base';
+
+//estilo
+import styles from './style';
+import LogoTitle from '../../components/LogoTitle/Index'
+//components/LogoTitle'
+
+export default class Search extends React.Component {
+    state = {
+        list: [],
+        error: false
+    };
+
+    static navigationOptions = {
+      headerTitle: <LogoTitle/>
+    }
+
+    constructor(props) {
+        super(props);
+        this.search = this.search.bind(this);
+    }
+
+    search(value) {
+        if (value.length > 3) {
+            this.setState({ list: [], error: false });
+
+            fetch(`https://www.superheroapi.com/api.php/10216378608089852/search/${value}`)
+                .then((response) => response.json())
+                .then((data) => {
+                    if (data.error) {
+                        return this.setState({ list: [], error: true });
+                    }
+                    const results = data.results;
+                    return this.setState({ list: results, error: false });
+                });
+        }
+    }
+
+    renderError() {
+        if (this.state.error) {
+            return (<Text>Ops!</Text>);
+        }
+        return null;
+    }
+
+    renderList() {
+        if (this.state.list.length === 0) {
+            return null;
+        }
+
+        return (<List>
+            {
+                this.state.list.map((item) => (
+                    <ListItem key={`item-${item.id}`}>
+                        <Left>
+                            <Thumbnail
+                                square
+                                small
+                                source={{ uri: item.image.url }}
+                            />
+                        </Left>
+                        <Body>
+                            <Text>{item.id}</Text>
+                            <Text>{item.name}</Text>
+                        </Body>
+                        <Right style={{ paddingLeft: 10 }}>
+                            <Button
+                                transparent
+                                dark
+                                onPress={
+                                    () => this.props.navigation.push('Profile', {
+                                        id: item.id,
+                                    })
+                                }
+                            >
+                                <Icon name="arrow-forward" />
+                            </Button>
+                        </Right>
+                    </ListItem>
+                ))
+            }
+        </List>)
+    }
+
+    render() {
+        return (
+            <View style={styles.container}>
+                <TextInput
+                    style={styles.textInput}
+                    onChangeText={this.search}
+                />
+
+                {this.renderList()}
+                {this.renderError()}
+
+            </View>
+        );
+    }
+}
